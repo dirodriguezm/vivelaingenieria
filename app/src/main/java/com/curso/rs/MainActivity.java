ad8809bd43b79962ad813715893d4f2849e3e127
meraioth <mb.us.94@gmail.com>
2017-04-21 16:46:30 -0300

    canvas y cuadrados

--- app/src/main/java/com/curso/rs/MainActivity.java
+++ app/src/main/java/com/curso/rs/MainActivity.java
@@ -21,43 +21,30 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-
-// We'll be creating an image that is 100 pixels wide and 200 pixels tall.
         int width = (int) ((int)metrics.widthPixels * 0.9);
         int height = (int) ((int)metrics.heightPixels * 0.25);
 
 
-// Create a bitmap with the dimensions we defined above, and with a 16-bit pixel format. We'll
-// get a little more in depth with pixel formats in a later post.
+
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
 
-// Create a paint object for us to draw with, and set our drawing color to blue.
         Paint paint = new Paint();
         paint.setColor(Color.BLUE);
 
-// Create a new canvas to draw on, and link it to the bitmap that we created above. Any drawing
-// operations performed on the canvas will have an immediate effect on the pixel data of the
-// bitmap.
         Canvas canvas = new Canvas(bitmap);
 
-// Fill the entire canvas with a red color.
         canvas.drawColor(Color.GREEN);
         int margen=5;
         int separacion=5;
         int width_f=width-margen;
-// Draw a rectangle inside our image using the paint object we defined above. The rectangle's
-// upper left corner will be at (25,50), and the lower left corner will be at (75,150). Since we set
-// the paint object's color above, this rectangle will be blue.
         ArrayList rect = new ArrayList();
 
         rect.add(new Rect(margen,margen,margen+width_f/9,height-margen));
 
 
-        for(int i=1;i<=8;i++) {
-            rect.add(new Rect(separacion+margen+width_f/9*i,margen,margen+width_f/9*(i+1),height-margen));
-        }
-// In order to display this image in our activity, we need to create a new ImageView that we
-// can display.
+//        for(int i=1;i<=8;i++) {
+//            rect.add(new Rect(separacion+margen+width_f/9*i,margen,margen+width_f/9*(i+1),height-margen));
+//        }
         for (Object r:rect
              ) {
            canvas.drawRect((Rect)r,paint);
