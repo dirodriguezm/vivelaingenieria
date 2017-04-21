b9adfae2ec09d239385731c4b0780edf62140bd3
meraioth <mb.us.94@gmail.com>
2017-04-21 18:07:26 -0300

    canvas y cuadrados

--- app/src/main/java/com/curso/rs/MainActivity.java
+++ app/src/main/java/com/curso/rs/MainActivity.java
@@ -5,19 +5,25 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Button iniciar,reset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        iniciar = (Button) findViewById(R.id.button);
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
@@ -28,41 +34,60 @@ public class MainActivity extends AppCompatActivity {
 
         Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
 
-        Paint paint = new Paint();
+        final Paint paint = new Paint();
         paint.setColor(Color.BLUE);
 
-        Canvas canvas = new Canvas(bitmap);
+        final Canvas canvas = new Canvas(bitmap);
 
         canvas.drawColor(Color.GREEN);
         int margen=5;
         int separacion=5;
         int width_f=width-margen;
-        ArrayList rect = new ArrayList();
+        final ArrayList rect = new ArrayList();
 
         rect.add(new Rect(margen,margen,margen+width_f/9,height-margen));
 
 
-//        for(int i=1;i<=8;i++) {
-//            rect.add(new Rect(separacion+margen+width_f/9*i,margen,margen+width_f/9*(i+1),height-margen));
-//        }
+        for(int i=1;i<=8;i++) {
+            rect.add(new Rect(separacion+margen+width_f/9*i,margen,margen+width_f/9*(i+1),height-margen));
+        }
+        int count=0;
         for (Object r:rect
              ) {
-           canvas.drawRect((Rect)r,paint);
+            if(count<4) {
+
+                canvas.drawRect((Rect) r, new Paint(Color.rgb(94, 62, 1)));
+
+            }else if(count!=4){
+                canvas.drawRect((Rect) r, new Paint(Color.rgb(96, 119, 1)));
+            }else
+                canvas.drawRect((Rect) r, new Paint(Color.WHITE));
+
+            count++;
         }
-        ImageView imageView = new ImageView(this);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
 
 // Set this ImageView's bitmap to the one we have drawn to.
         imageView.setImageBitmap(bitmap);
+        iniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                paint.setColor(Color.BLACK);
+                canvas.drawRect((Rect)rect.get(4),paint);
+            }
+        });
 
 // Create a simple layout and add our image view to it.
-        RelativeLayout layout = new RelativeLayout(this);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        params.addRule(RelativeLayout.CENTER_IN_PARENT);
-        layout.addView(imageView, params);
-        layout.setBackgroundColor(Color.WHITE);
+//        RelativeLayout layout = (RelativeLayout)findViewById(R.id.principal);
+//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+//                RelativeLayout.LayoutParams.WRAP_CONTENT);
+//        //params.addRule(RelativeLayout.CENTER_IN_PARENT);
+//        layout.addView(imageView,params);
+//        layout.setBackgroundColor(Color.WHITE);
+//        setContentView(layout);
 
 // Show this layout in our activity.
-        setContentView(layout);
+
     }
 }
