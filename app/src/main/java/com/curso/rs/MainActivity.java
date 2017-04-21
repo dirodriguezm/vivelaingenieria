6e246c216f49f1a6946dd5e188db51f2004b4c8f
meraioth <mb.us.94@gmail.com>
2017-04-21 16:41:26 -0300

    canvas y cuadrados

--- app/src/main/java/com/curso/rs/MainActivity.java
+++ app/src/main/java/com/curso/rs/MainActivity.java
@@ -4,21 +4,28 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
 
 // We'll be creating an image that is 100 pixels wide and 200 pixels tall.
-        int width = 100;
-        int height = 200;
+        int width = (int) ((int)metrics.widthPixels * 0.9);
+        int height = (int) ((int)metrics.heightPixels * 0.25);
+
 
 // Create a bitmap with the dimensions we defined above, and with a 16-bit pixel format. We'll
 // get a little more in depth with pixel formats in a later post.
@@ -34,15 +41,27 @@ public class MainActivity extends AppCompatActivity {
         Canvas canvas = new Canvas(bitmap);
 
 // Fill the entire canvas with a red color.
-        canvas.drawColor(Color.RED);
-
+        canvas.drawColor(Color.GREEN);
+        int margen=5;
+        int separacion=5;
+        int width_f=width-margen;
 // Draw a rectangle inside our image using the paint object we defined above. The rectangle's
 // upper left corner will be at (25,50), and the lower left corner will be at (75,150). Since we set
 // the paint object's color above, this rectangle will be blue.
-        canvas.drawRect(25, 50, 75, 150, paint);
+        ArrayList rect = new ArrayList();
 
+        rect.add(new Rect(margen,margen,margen+width_f/9,height-margen));
+
+
+        for(int i=1;i<=8;i++) {
+            rect.add(new Rect(separacion+margen+width_f/9*i,margen,margen+width_f/9*(i+1),height-margen));
+        }
 // In order to display this image in our activity, we need to create a new ImageView that we
 // can display.
+        for (Object r:rect
+             ) {
+           canvas.drawRect((Rect)r,paint);
+        }
         ImageView imageView = new ImageView(this);
 
 // Set this ImageView's bitmap to the one we have drawn to.
